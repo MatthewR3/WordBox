24b389a4e4d173c3e3529ef15eeff3d9d0a87225
Matthew <matthewrastovac@yahoo.com>
2017-08-05 16:47:52 -0400

    Minor script change

--- sample_utterances/random_words.py
+++ sample_utterances/random_words.py
@@ -1,6 +1,6 @@
 # Generates sample utterances for WordWorks based on 1,000 of the most common words in the English language
 
-with open("D:/Programming/WordBox/freq.txt", "r") as f:
+import os
+with open(os.path.dirname(os.path.realpath(__file__)) + "/freq.txt", "r") as f:
 	for line in f:
-		# print line.strip()
 		print "GetRhymeIntent rhyme for {" + line.strip() + "|Word}"
