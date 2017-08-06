c6a39e0dfba4387723448b8be446c697e9450604
Matthew <matthewrastovac@yahoo.com>
2017-08-06 13:34:33 -0400

    Added custom slot type to interaction model json

--- sample_utterances/model_json.py
+++ sample_utterances/model_json.py
@@ -16,25 +16,34 @@ import os
 import json
 
 
-with open(os.path.dirname(os.path.realpath(__file__)) + "/freq.txt", "r") as f:
+dev = 1
+
+input_file = "freq_small.txt" if dev else "freq.txt"
+output_file = "sample_utterances_small.txt" if dev else "sample_utterances.txt"
+
+with open(os.path.dirname(os.path.realpath(__file__)) + "/freq_small.txt", "r") as f:
 	all_words = [line.strip() for line in f]
 
 custom_intents = [
-			("GetSynonymIntent", ("a synonym for",)),
-			("GetAntonymIntent", ("an antonym for",)),
-			("GetPOSIntent", ("the part of speech for",)),
-			("GetRhymeIntent", ("a rhyme for",)),
-			("GetDefinitionIntent", ("the definition of",)),
-			("GetDefinitionIntent", ("define",)),
-			("GetSyllablesIntent", ("the syllables for",)),
-			("GetSyllablesIntent", ("the syllables of",)),
-			("GetFrequencyIntent", ("the frequency of",)),
-			("GetFrequencyIntent", ("how common", "is")),
-			("GetPronunciationIntent", ("how to pronounce",)),
-			("GetPronunciationIntent", ("pronunciation of",))
+	("GetSynonymIntent", ("a synonym for",)),
+	("GetAntonymIntent", ("an antonym for",)),
+	("GetPOSIntent", ("the part of speech for",)),
+	("GetRhymeIntent", ("a rhyme for",)),
+	("GetDefinitionIntent", ("the definition of",)),
+	("GetDefinitionIntent", ("define",)),
+	("GetSyllablesIntent", ("the syllables for",)),
+	("GetSyllablesIntent", ("the syllables of",)),
+	("GetFrequencyIntent", ("the frequency of",)),
+	("GetFrequencyIntent", ("how common", "is")),
+	("GetPronunciationIntent", ("how to pronounce",)),
+	("GetPronunciationIntent", ("pronunciation of",))
 ]
 
-intent_json = {"intents": []}
+custom_slots = [
+	("LITERAL", all_words)
+]
+
+intent_json = {"intents": [], "types": []}
 intent_json["intents"].append({"name": "AMAZON.CancelIntent", "samples": []})
 intent_json["intents"].append({"name": "AMAZON.HelpIntent", "samples": ["help"]})
 intent_json["intents"].append({"name": "AMAZON.StopIntent", "samples": ["cancel"]})
@@ -52,7 +61,11 @@ for intent, utterance in custom_intents:
 			intent_exists = True
 			break
 	if not intent_exists:
-		intent_json["intents"].append({"name": intent, "samples": all_samples})
+		intent_json["intents"].append({"name": intent, "samples": all_samples, "slots": [{"name": "Word", "type": "LITERAL", "samples": []}]})
 
-with open(os.path.dirname(os.path.realpath(__file__)) + "/sample_utterances.json", "w") as f:
+for slot, values in custom_slots:
+	intent_json["types"].append({"name": slot, "values": values})
+
+
+with open(os.path.dirname(os.path.realpath(__file__)) + "/sample_utterances_small.json", "w") as f:
 	json.dump(intent_json, f)
